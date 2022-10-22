5d7210fa920e07f984d68b36aa03590430595bd0
Hemanth Reddy M <115568561+Mhrgithub22@users.noreply.github.com>
2022-10-22 19:00:20 +0530

    Add files via upload

---
+++ PatternProgramming2.java
@@ -0,0 +1,30 @@
+package in.ineuron.main;
+
+public class PatternProgramming2 {
+
+	public static void main(String[] args) {
+
+		
+		for (int i=1 ;i<5;i++)
+		{
+			
+			for ( int j=1 ;j<5;j++)
+			{
+				System.out.print(i);
+			}
+			System.out.println( );
+		}
+		
+		
+		
+			
+			
+//			if ( i==0 )
+//				System.out.print("1");
+	    
+			
+		}
+		
+	}
+
+
